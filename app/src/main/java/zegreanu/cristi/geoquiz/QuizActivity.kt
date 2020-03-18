5b52b417c6165bdc33528f00f217874000ffe8ae
czegreanu <cristi.zegreanu@fortech.ro>
2020-03-18 15:23:56 +0200

    Finish first chapter and code improvements

--- app/src/main/java/zegreanu/cristi/geoquiz/QuizActivity.kt
+++ app/src/main/java/zegreanu/cristi/geoquiz/QuizActivity.kt
@@ -1,12 +1,31 @@
 package zegreanu.cristi.geoquiz
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class QuizActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+
+        val mTrueButton = findViewById<Button>(R.id.true_button)
+        mTrueButton?.setOnClickListener {
+            Toast.makeText(
+                this@QuizActivity,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        val mFalseButton = findViewById<Button>(R.id.false_button)
+        mFalseButton?.setOnClickListener {
+            Toast.makeText(
+                this@QuizActivity,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
