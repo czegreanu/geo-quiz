228028a594955a737e6e68ce0d4c74260816b72a
czegreanu <cristi.zegreanu@fortech.ro>
2020-03-18 15:46:28 +0200

    Customize toast (challenge)

--- app/src/main/java/zegreanu/cristi/geoquiz/QuizActivity.kt
+++ app/src/main/java/zegreanu/cristi/geoquiz/QuizActivity.kt
@@ -1,6 +1,7 @@
 package zegreanu.cristi.geoquiz
 
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -16,7 +17,10 @@ class QuizActivity : AppCompatActivity() {
                 this@QuizActivity,
                 R.string.correct_toast,
                 Toast.LENGTH_SHORT
-            ).show()
+            ).run {
+                setGravity(Gravity.TOP, 0, 0)
+                show()
+            }
         }
 
         val mFalseButton = findViewById<Button>(R.id.false_button)
@@ -25,7 +29,10 @@ class QuizActivity : AppCompatActivity() {
                 this@QuizActivity,
                 R.string.incorrect_toast,
                 Toast.LENGTH_SHORT
-            ).show()
+            ).run {
+                setGravity(Gravity.TOP, 0, 0)
+                show()
+            }
         }
     }
 }
