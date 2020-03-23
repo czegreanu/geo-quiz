b014133517944e5a41cbae5726b743804d4d44df
czegreanu <cristi.zegreanu@gmail.com>
2020-03-23 12:38:47 +0200

    Chapter 6 implementation
    Add detekt, add animation

--- app/src/main/java/zegreanu/cristi/geoquiz/CheatActivity.kt
+++ app/src/main/java/zegreanu/cristi/geoquiz/CheatActivity.kt
@@ -2,10 +2,14 @@ package zegreanu.cristi.geoquiz
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.ViewAnimationUtils
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
 
 class CheatActivity : AppCompatActivity() {
     private lateinit var showAnswerButton: Button
@@ -27,8 +31,31 @@ class CheatActivity : AppCompatActivity() {
                 answerTextView.setText(R.string.true_button)
             else
                 answerTextView.setText(R.string.false_button)
+
             setAnswerShownResult(true)
+            showAnimation()
         }
+    }
+
+    private fun showAnimation() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val cx: Int = showAnswerButton.width / 2
+            val cy: Int = showAnswerButton.height / 2
+            val radius: Float = showAnswerButton.width.toFloat()
+            val anim =
+                ViewAnimationUtils.createCircularReveal(
+                    showAnswerButton,
+                    cx,
+                    cy,
+                    radius,
+                    0f
+                )
+            anim.apply {
+                doOnEnd { showAnswerButton.visibility = View.INVISIBLE }
+                start()
+            }
+        } else
+            showAnswerButton.visibility = View.INVISIBLE
     }
 
     private fun setAnswerShownResult(isAnswerShown: Boolean) {
@@ -41,7 +68,7 @@ class CheatActivity : AppCompatActivity() {
         val EXTRA_ANSWER_SHOWN = "zegreanu.cristi.geoquiz.answer_shown"
 
         fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
-            var intent = Intent(packageContext, CheatActivity::class.java)
+            val intent = Intent(packageContext, CheatActivity::class.java)
             intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
 
             return intent
